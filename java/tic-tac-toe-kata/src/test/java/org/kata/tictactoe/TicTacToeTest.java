9283a0dcd9b03112f25159adb39446f32b86561f
Alvaro Garcia <derecerca@gmail.com>
2018-03-15 14:02:35 +0100

    Red

--- java/tic-tac-toe-kata/src/test/java/org/kata/tictactoe/TicTacToeTest.java
+++ java/tic-tac-toe-kata/src/test/java/org/kata/tictactoe/TicTacToeTest.java
@@ -77,17 +77,6 @@ public class TicTacToeTest {
         assertThat(winning("X", board)).isEqualTo(true);
     }
 
-    @Test
-    public void win_in_third_row() {
-        String[][] board = new String[][] { { "", "", "" }, { "", "", "" }, { "", "", "" } };
-
-        set("X", board, 2, 0);
-        set("X", board, 2, 1);
-        set("X", board, 2, 2);
-
-        assertThat(winning("X", board)).isEqualTo(true);
-    }
-
     private boolean winning(String p, String[][] b) {
 
         return inColumn(p, b, 0) || inColumn(p, b, 1) || inColumn(p, b, 2) || inRow(p, b, 0) || inRow(p, b, 0) || inRow(p, b, 0);
