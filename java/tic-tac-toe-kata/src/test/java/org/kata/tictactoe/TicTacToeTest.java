3457a66ce2f1baa867b1ffdc1e34278b085f63b0
Alvaro Garcia <derecerca@gmail.com>
2018-03-15 13:44:59 +0100

    Red

--- java/tic-tac-toe-kata/src/test/java/org/kata/tictactoe/TicTacToeTest.java
+++ java/tic-tac-toe-kata/src/test/java/org/kata/tictactoe/TicTacToeTest.java
@@ -1,5 +1,19 @@
 package org.kata.tictactoe;
 
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class TicTacToeTest {
 
+    @Test
+    public void is_empty() {
+
+        String[][] board = new String[0][0];
+        assertThat(empty(board)).isEqualTo(true);
+    }
+
+    private boolean empty(String[][] board) {
+        return false;
+    }
 }
