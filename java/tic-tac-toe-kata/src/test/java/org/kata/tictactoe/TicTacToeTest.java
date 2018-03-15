2c1c9c88ca5f4f64e601d38c7e92bd4734525fd7
Alvaro Garcia <derecerca@gmail.com>
2018-03-15 13:48:00 +0100

    Green

--- java/tic-tac-toe-kata/src/test/java/org/kata/tictactoe/TicTacToeTest.java
+++ java/tic-tac-toe-kata/src/test/java/org/kata/tictactoe/TicTacToeTest.java
@@ -9,8 +9,21 @@ public class TicTacToeTest {
     @Test
     public void is_empty() {
         String[][] board = new String[0][0];
-        
+
         assertThat(empty(board)).isEqualTo(true);
+    }
+
+    @Test
+    public void sets_the_position() {
+        String[][] board = new String[1][1];
+
+        set("X", board, 0, 0);
+
+        assertThat(board[0][0]).isEqualTo("X");
+    }
+
+    private void set(String x, String[][] board, int i, int j) {
+        board[i][j] = x;
     }
 
     private boolean empty(String[][] board) {
