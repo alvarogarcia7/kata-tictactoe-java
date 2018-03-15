22480499e14546b1b77d7511bfb7f24118bc4d1c
Alvaro Garcia <derecerca@gmail.com>
2018-03-15 13:43:43 +0100

    FAILED
     Revert "Red"
    
    This reverts commit bc6866782a5348d1968d1f8e6db9b281b0cba5f8.

--- java/tic-tac-toe-kata/src/test/java/org/kata/tictactoe/TicTacToeTest.java
+++ java/tic-tac-toe-kata/src/test/java/org/kata/tictactoe/TicTacToeTest.java
@@ -1,18 +1,5 @@
 package org.kata.tictactoe;
 
-import org.junit.Test;
-
-import static org.assertj.core.api.Java6Assertions.assertThat;
-
 public class TicTacToeTest {
-    @Test
-    public void the_board_is_empty() {
-        String[][] board = new String[0][0];
 
-        assertThat(isEmpty(board)).isEqualTo(true);
-    }
-
-    private boolean isEmpty(String[][] board) {
-        return false;
-    }
 }
